da13572fbf7e21a0e71e24026d243a625bf406a1
quduss <shuaibqudus33@gmail.com>
2024-08-11 11:50:30 +0100

    Refactor script

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,57 +1,54 @@
 $(document).ready(function () {
-    const checkedAmenities = {};
-    const amenities = [];
-    $('input:checkbox').change(function () {
-      const id = $(this).data('id');
-      const name = $(this).data('name');
-      const amn = $(this).data('am');
-      if ($(this).is(':checked')) {
-        checkedAmenities[id] = name;
-        amenities.push(amn);
+  let checkedAmenities = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      checkedAmenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete checkedAmenities[$(this).data('id')];
+    }
+    let lst = Object.values(checkedAmenities);
+    if (lst.length > 0) {
+      $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
       } else {
-        delete checkedAmenities[id];
-        amenities.pop(amn);
+        $('#api_status').removeClass('available');
       }
-      const res = Object.values(checkedAmenities);
-      if (res.length > 0) {
-        $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
-      } else {
-        $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        let place = data[i];
+        $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
       }
-    });
-  
+    }
+  });
+  $('.filters > button').click(function () {
+    $('.places > article').remove();
     $.ajax({
-      url: 'http://0.0.0.0:5001/api/v1/status/',
-      type: 'GET',
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      data: JSON.stringify({'amenities': Object.keys(checkedAmenities)}),
+      dataType: 'json',
+      contentType: 'application/json',
       success: function (data) {
-        if (data.status === 'OK') {
-          $('header div#api_status').addClass('available');
-        } else {
-          if ($('header div#api_status').hasClass('available')) {
-            $('header div#api_status').removeClass('available');
-          }
+        for (let i = 0; i < data.length; i++) {
+          let place = data[i];
+          $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
         }
       }
-    });
-  
-    $('section.filters button').click(function () {
-      $.ajax({
-        url: 'http://0.0.0.0:5001/api/v1/places_search/',
-        data: JSON.stringify({ amenities }),
-        contentType: 'application/json; charset=utf-8',
-        type: 'POST',
-        success: function (data) {
-          $.each(data, function (i, obj) {
-            $('section.places').append('<article><div class="headline"><h2>' +
-            obj.name + '</h2><div class="price_by_night">' +
-            obj.price_by_night + '$</div></div>' + '<div class="information">' +
-            '<div class="max_guest">' + '<div  class="guest_icon"></div>' + '<p>' +
-            obj.max_guest + '</p></div><div class="number_rooms">' + '<div class="bed_icon"></div><p>' +
-            obj.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_icon"></div><p>' +
-            obj.number_bathrooms + '</p></div></div><div class="description"><p>' +
-            obj.description + '| safe' + '</p></div></article>');
-          });
-        }
-      });
     });
   });
+});
